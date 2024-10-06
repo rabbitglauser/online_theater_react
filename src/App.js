4cd55fb0343717d7102c46bc6928cd60b5012eb9
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-10-06 21:55:38 +0200

    Refactor CSS and implement new navbar
    Moved CSS files to a 'styles' directory and updated imports accordingly. Replaced the old header with a new navbar, including links and icons for cart and user account. Added FontAwesome for iconography.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav className="navbar">
+                <div className="navbar-left">
+                    <a href="/" className="logo">Kino Zug</a>
+                </div>
+                <div className="navbar-center">
+                    <ul className="nav-links">
+                        <li>
+                            <a href="/Program">Products</a>
+                        </li>
+                        <li>
+                            <a href="/uber-uns">Über uns</a>
+                        </li>
+                        <li>
+                            <a href="/Preise">Preise</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="navbar-right">
+                    <a href="/cart" className="cart-icon">
+                        <i className="fas fa-shopping-cart"></i>
+                        <span className="cart-count">0</span>
+                    </a>
+                    <a href="/account" className="user-icon">
+                        <i className="fas fa-user"></i>
+                    </a>
+                </div>
+            </nav>
+        </div>
+    );
 }
 
 export default App;
+
